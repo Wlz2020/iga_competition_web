1e6462d912a7bb3dd15833e230e5ea00a2e2fb01
wlz2022 <2667461198@qq.com>
2024-06-01 15:53:55 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,6 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VueDevTools()],
-  server: {
-    host: '0.0.0.0',
-    port: 3001
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
