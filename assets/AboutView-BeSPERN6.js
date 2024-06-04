581bd620a2062808d9377bf77d89f6fad9885888
wlz2022 <2667461198@qq.com>
2024-06-04 13:06:19 +0800

    commit

--- assets/AboutView-BeSPERN6.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/AboutView-BhL1GC3P.js
-import{_ as e,c as o,o as s,a as t}from"./index-BFRKK26S.js";const c={},a={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return s(),o("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
-========
-import{_ as e,c as o,o as s,a as t}from"./index-OGfTTHek.js";const c={},a={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return s(),o("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
->>>>>>>> 96a2ceb2ed505da10efeaa2202d6562c809bf9db:dist/assets/AboutView-BeSPERN6.js
